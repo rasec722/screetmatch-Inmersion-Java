4a89fa08c2ad1f0bf43129a301e21391ccff9d74
Cesar <cesar.r1674@gmail.com>
2024-05-05 21:46:01 -0600

    aula 02 inmersion java

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int fechaPelicula = 1999;
+        int fechaReciente = 2023;
+        double evaluacion =4.5;
+        double mediaEvaluacion = (4.5+4.8+3)/3;
+        double mediaEvaluacionUsuario = 0;
+        boolean incluyePlanBasico = true;
+        String nombre = "Pelicula n";
+        String sinopsis = "";
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Pelicula: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaPelicula);
+        System.out.println("Valoracion de la pelicula: " + evaluacion);
+        System.out.println("Permiso de acceso a la pelicula:"+ incluyePlanBasico);
+        System.out.println("Media de evaluacion: " + mediaEvaluacion);
+
+        if(fechaPelicula >= fechaReciente){
+            System.out.println("Pelicula popular");
+        }else{
+            System.out.println("Pelicula retro que vale la pena ver");
+        }
+        for(int i=0; i <3; i++){
+            System.out.println("Ingresa la nota que le das a la pelicula N: ");
+            double notaUsuario = sc.nextDouble();
+            mediaEvaluacionUsuario = mediaEvaluacionUsuario+notaUsuario;
+        }
+        System.out.println("La media de la pelicula por el usuario:"+ mediaEvaluacionUsuario/3);
+    }
+
+
+
+
+
+}
